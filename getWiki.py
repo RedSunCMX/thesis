b856ee9316f2b56d5116bceb9f7416262cfc4330
dvdgrs <dpgraus@gmail.com>
2011-09-01 19:55:41 +0200

    move getWiki

---
+++ getWiki.py
@@ -0,0 +1,12 @@
+from wikitools import wiki,api
+from pprint import pprint
+import nltk
+
+def get(title):
+	title = "'" + title + "'"
+	site = wiki.Wiki('http://en.wikipedia.org/w/api.php')
+	params = {'action':'query', 'prop':'revisions', 'rvprop':'content', 'format':'xml', 'titles':title }
+	request = api.APIRequest(site,params)
+	result = request.query()
+	clean = nltk.clean_html(str(result))
+	pprint(clean)
